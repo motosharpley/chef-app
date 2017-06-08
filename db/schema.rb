beec60f3fd8ccae35f5a9d96cefd768f8af63796
motosharpley <motosharpley@gmail.com>
2017-06-08 16:27:03 +0000

    add ingredient model and m to m associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607171035) do
+ActiveRecord::Schema.define(version: 20170608160740) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
