95159c974a6ba11a4d175e3b7d220c78fd27a9aa
motosharpley <motosharpley@gmail.com>
2017-06-08 18:25:33 +0000

    add comment model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608160740) do
+ActiveRecord::Schema.define(version: 20170608165403) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
