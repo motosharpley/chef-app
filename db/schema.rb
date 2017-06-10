a51d403ed1eaaeffd93a74db64364e00f9a992ec
motosharpley <motosharpley@gmail.com>
2017-06-10 20:47:01 +0000

    build chat feature and connect to actioncable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608165403) do
+ActiveRecord::Schema.define(version: 20170610173331) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "description"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170608165403) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipe_ingredients", force: :cascade do |t|
